3684bd355cd343d4cd6d23c350a98969bda85042
Thiago Bardez <bardez@MacBook-Air-de-Thiago.local>
2020-04-19 23:21:55 -0300

    * Including logo; * Adding scripts on template; * Creating Template for Occupation list and Register Form Adding css changes for style

--- components/header.php
+++ components/header.php
@@ -1,5 +1,13 @@
-<header class='container'>
-    <a href="<?php echo DEFAULT_URL;?>index.php" class="btn btn-dark"> HOME</a>
-    <a href="<?php echo DEFAULT_URL;?>listing/occupation.php" class="btn btn-dark"> LISTA</a>
-    <a href="<?php echo DEFAULT_URL;?>registration/occupation.php" class="btn btn-dark">CADASTRO</a>
+<header>
+    <img src="/resources/img/logo.png" alt="P.E.F" height="80px">
+    <a href="<?php echo DEFAULT_URL;?>index.php" class="btn btn-dark"> HOME</a> 
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDProfissoes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            PROFISSÕES
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDProfissoes'>
+            <a href="<?php echo DEFAULT_URL;?>listing/occupation.php" class="dropdown-item" >Mostrar todas</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/occupation.php" class="dropdown-item" >Nova Profissão</a>
+        </div>
+    </div>
 </header>
--- template.php
+++ template.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>P.E.F</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/styles/global.css">
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php include('../config/connection.php'); ?>
