24a6c740389dc0b35236ddb5ac60036a16662922
Renam Mello da Silva <renammello5454@gmail.com>
2020-04-22 01:20:08 -0300

    commmit

--- config/constants.php
+++ config/constants.php
@@ -3,9 +3,9 @@
     define('HOST', '127.0.0.1');
     define('DATABASE', 'pef');
     define('USER', 'root');
-    define('PASSWORD', 'bardez');
-    define('PORT', '3306');
+    define('PASSWORD', 'renam5858');
+    define('PORT', '3307');
 
     // SYSTEM VARS
-    define('DEFAULT_URL', 'http://localhost:5000/');
+    define('DEFAULT_URL', 'http://localhost:8000/');
 ?>
