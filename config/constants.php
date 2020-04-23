195e9e01e0ed151ecb21e927475fa6cadb2ead48
Lucas Renatto <lucasrenattotmz07@hotmail.com>
2020-04-22 21:30:49 -0300

    test deffss

--- config/constants.php
+++ config/constants.php
@@ -3,9 +3,9 @@
     define('HOST', '127.0.0.1');
     define('DATABASE', 'pef');
     define('USER', 'root');
-    define('PASSWORD', 'cebola');
+    define('PASSWORD', '123');
     define('PORT', '3306');
-
+    <select name="ss" id="ss"></select>
     // SYSTEM VARS
-    define('DEFAULT_URL', 'http://localhost:8000/');//asdasdf
+    define('DEFAULT_URL', 'http://localhost:5001/');//asdasdf
 ?>
