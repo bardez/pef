882dd69c41c0804e58b09079f3a7b3185d30b86a
Thiago Bardez <thiagobardez@gmail.com>
2020-04-26 22:25:42 -0300

    adjusting header css

--- components/header.php
+++ components/header.php
@@ -2,35 +2,23 @@
     <img src="/resources/img/logo.png" alt="P.E.F" height="80px">
     <a href="<?php echo DEFAULT_URL;?>index.php" class="btn btn-dark"> HOME</a> 
     <div class="dropdown">
-        <button class="btn btn-dark dropdown-toggle" type="button" id="DDProfissoes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            PROFISSÕES
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDSubject" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            ASSUNTO PRINCIPAL
         </button>
-        <div class="dropdown-menu" aria-labelledby='DDProfissoes'>
-            <a href="<?php echo DEFAULT_URL;?>listing/occupation.php" class="dropdown-item" >Mostrar todas</a>
-            <a href="<?php echo DEFAULT_URL;?>registration/occupation.php" class="dropdown-item" >Nova Profissão</a>
-        </div>
-    </div>
-
-    <div class="dropdown">
-        <button class="btn btn-dark dropdown-toggle" type="button" id="DDProfissoes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Assunto
-        </button>
-        <div class="dropdown-menu" aria-labelledby='DDProfissoes'>
+        <div class="dropdown-menu" aria-labelledby='DDSubject'>
             <a href="<?php echo DEFAULT_URL;?>listing/subjectmatter.php" class="dropdown-item" >Mostrar todas</a>
-            <a href="<?php echo DEFAULT_URL;?>registration/subjectmatter.php" class="dropdown-item" >Nova Profissão</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/subjectmatter.php" class="dropdown-item" >Novo Assunto</a>
         </div>
     </div>
-
     <div class="dropdown">
-        <button class="btn btn-dark dropdown-toggle" type="button" id="DDForo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            FORO
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDClasse_processo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            CLASSES DE PROCESSOS
         </button>
-        <div class="dropdown-menu" aria-labelledby='DDForo'>
-            <a href="<?php echo DEFAULT_URL;?>listing/forum.php" class="dropdown-item" >Mostrar todos</a>
-            <a href="<?php echo DEFAULT_URL;?>registration/forum.php" class="dropdown-item" >Novo Foro</a>
+        <div class="dropdown-menu" aria-labelledby='DDClasse_processo'>
+            <a href="<?php echo DEFAULT_URL;?>listing/class_process.php" class="dropdown-item" >Mostrar todos</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/class_process.php" class="dropdown-item" >Nova Classe de processo</a>
         </div>
     </div>
-
     <div class="dropdown">
         <button class="btn btn-dark dropdown-toggle" type="button" id="DDCompetencias" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             COMPETÊNCIA
@@ -40,7 +28,42 @@
             <a href="<?php echo DEFAULT_URL;?>registration/competence.php" class="dropdown-item" >Nova Competência</a>
         </div>
     </div>
-
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDDocumento" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            DOCUMENTO
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDDocumento'>
+            <a href="<?php echo DEFAULT_URL;?>listing/document.php" class="dropdown-item" >Mostrar todos</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/document.php" class="dropdown-item" >Novo Documento</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDEstado_civil" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            ESTADO CIVIL
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDEstado_civil'>
+            <a href="<?php echo DEFAULT_URL;?>listing/civil_state.php" class="dropdown-item" >Mostrar todos</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/civil_state.php" class="dropdown-item" >Novo Estado Civil</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDForo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            FORO
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDForo'>
+            <a href="<?php echo DEFAULT_URL;?>listing/forum.php" class="dropdown-item" >Mostrar todos</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/forum.php" class="dropdown-item" >Novo Foro</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDNacionalidade" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            NACIONALIDADE
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDNacionalidade'>
+            <a href="<?php echo DEFAULT_URL;?>listing/nacionality.php" class="dropdown-item" >Mostrar todas</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/nacionality.php" class="dropdown-item" >Nova Nacionalidade</a>
+        </div>
+    </div>
     <div class="dropdown">
         <button class="btn btn-dark dropdown-toggle" type="button" id="DDPartes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             PARTES
@@ -50,25 +73,40 @@
             <a href="<?php echo DEFAULT_URL;?>registration/part.php" class="dropdown-item" >Nova Parte</a>
         </div>
     </div>
-
     <div class="dropdown">
-        <button class="btn btn-dark dropdown-toggle" type="button" id="DDProfissoes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            DOCUMENTOS
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDParticipacao" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            PARTICIPACAO
         </button>
-        <div class="dropdown-menu" aria-labelledby='DDProfissoes'>
-            <a href="<?php echo DEFAULT_URL;?>listing/document.php" class="dropdown-item" >Mostrar todos</a>
-            <a href="<?php echo DEFAULT_URL;?>registration/document.php" class="dropdown-item" >Novo Documento</a>
+        <div class="dropdown-menu" aria-labelledby='DDParticipacao'>
+            <a href="<?php echo DEFAULT_URL;?>listing/participation.php" class="dropdown-item" >Mostrar todas</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/participation.php" class="dropdown-item" >Nova Participacao</a>
         </div>
     </div>
-
-    
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDPeticao" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            PETIÇÃO INICIAL
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDPeticao'>
+            <a href="<?php echo DEFAULT_URL;?>listing/application.php" class="dropdown-item" >Mostrar todas</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/application.php" class="dropdown-item" >Nova Petição</a>
+        </div>
+    </div>
     <div class="dropdown">
         <button class="btn btn-dark dropdown-toggle" type="button" id="DDProfissoes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            CLASSES DE PROCESSOS
+            PROFISSÕES
         </button>
         <div class="dropdown-menu" aria-labelledby='DDProfissoes'>
-            <a href="<?php echo DEFAULT_URL;?>listing/class_process.php" class="dropdown-item" >Mostrar todos</a>
-            <a href="<?php echo DEFAULT_URL;?>registration/class_process.php" class="dropdown-item" >Nova Classe de processo</a>
+            <a href="<?php echo DEFAULT_URL;?>listing/occupation.php" class="dropdown-item" >Mostrar todas</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/occupation.php" class="dropdown-item" >Nova Profissão</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-dark dropdown-toggle" type="button" id="DDTipo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            TIPO
+        </button>
+        <div class="dropdown-menu" aria-labelledby='DDTipo'>
+            <a href="<?php echo DEFAULT_URL;?>listing/type.php" class="dropdown-item" >Mostrar todos</a>
+            <a href="<?php echo DEFAULT_URL;?>registration/type.php" class="dropdown-item" >Novo Tipo</a>
         </div>
     </div>
 </header>
