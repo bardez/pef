7bb16f877fa76dcfb69385efbcbde46b41b4a276
Thiago Bardez <thiagobardez@gmail.com>
2020-05-21 13:30:12 -0300

    Added login on header

--- components/header.php
+++ components/header.php
@@ -69,7 +69,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Top Menu Items</a>
                 </li> -->
-                <li class="nav-item">
+            </ul>
+            <ul class="navbar-nav navbar-right">
+            <li class="nav-item">
                 <form class="search" method="GET" action='/index.php'>
                     <div class="search__wrapper">
                         <input type="text" name="q" placeholder="Pesquisar" class="search__field">
@@ -77,6 +79,40 @@
                     </div>
                 </form>
                 </li> 
+                <li class="dropdown">
+                    <a class='btn btn-dark' href="http://www.jquery2dotnet.com" class="dropdown-toggle" data-toggle="dropdown">Entrar <b class="caret"></b></a>
+                    <ul class="dropdown-menu" style="padding: 15px;min-width: 250px; left: -165px;">
+                        <li>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputEmail2">Email</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputPassword2">Senha</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Senha" required>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                        <input type="checkbox"> Lembrar me
+                                        </label>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-success btn-block">Login</button>
+                                    </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <input class="btn btn-primary btn-block" type="button" id="sign-in-google" value="Login with Google">
+                            <input class="btn btn-primary btn-block" type="button" id="sign-in-twitter" value="Login with Twitter">
+                        </li>   
+                    </ul>
+                </li>
             </ul>
         </div>
     </nav>
